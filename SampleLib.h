d6c281c03301284f44f47fa281260afdd7d0086c
bounyamine <ousmanoubounyamine@gmail.com>
2025-01-15 12:07:05 +0100

    first commit

---
+++ SampleLib.h
@@ -0,0 +1,9 @@
+#ifndef __MY_MATH_LIB_H
+#define __MY_MATH_LIB_H
+#include "SampleLib.c"
+
+// Définition d'une fonction de calcul de factorielle.
+unsigned int fact( unsigned int value );
+// Définition d'une fonction d'élévation à une puissance données.
+int power( int value, unsigned int pow );
+#endif
